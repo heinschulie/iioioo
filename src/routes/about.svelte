b517960f6bb158b0d96d4245a6b8a0f02539c1e0
Hein <heinschulie@gmail.com>
2021-10-31 16:50:21 +0200

    added kindle highlights

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,52 +1,3 @@
-<!-- <script context="module">
-	import { browser, dev } from '$app/env';
-
-	// we don't need any JS on this page, though we'll load
-	// it in dev so that we get hot module replacement...
-	export const hydrate = dev;
-
-	// ...but if the client-side router is already loaded
-	// (i.e. we came here from elsewhere in the app), use it
-	export const router = browser;
-
-	// since there's no dynamic data here, we can prerender
-	// it so that it gets served as a static asset in prod
-	export const prerender = true;
-</script>
-
-<svelte:head>
-	<title>About</title>
-</svelte:head>
-
-<div class="content">
-	<h1>About this app</h1>
-
-	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
-	</p>
-
-	<pre>npm init svelte@next</pre>
-
-	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
-	</p>
-
-	<p>
-		The <a href="/todos">TODOs</a> page illustrates SvelteKit's data loading and form handling. Try using
-		it with JavaScript disabled!
-	</p>
-</div>
-
-<style>
-	.content {
-		width: 100%;
-		max-width: var(--column-width);
-		margin: var(--column-margin-top) auto 0 auto;
-	}
-</style> -->
 
 
 <!-- <script context="module">
@@ -288,3 +239,50 @@
 		color: white;
 	}
 </style> -->
+
+
+<script lang="ts">
+import { onMount } from "svelte";
+
+	let inputElement = document.getElementById("input");
+
+	function handleFiles(evt) {
+		
+		const file = evt.target.files[0]; 
+		
+		let reader = new FileReader();
+        reader.onload = (function(reader)
+        {
+            return function()
+            {
+                let contents = reader.result as string;
+				// console.log("CONTENTS: ", contents);
+                let lines = contents.split('==========').map( line => line.split("\r\n").filter(v => !!v) );
+                // console.log("LINES: ", lines); 
+				const objs = lines.reduce((acc, curr) => {
+					// console.log("CURR: ", curr); 
+					if(!curr.length) {
+						return acc; 
+					}
+					const author = curr[0].match(/\((.*?)\)/)[1];
+					const title = curr[0].split(`(${author})`)[0].trim();
+					const type = curr[1].includes("Note") ? "note" : "highlight"; 
+					const content = curr[2]; 
+					return [ ...acc, { title, author, type, content } ]; 
+				}, []);
+                //////
+                console.log("OBJS: ", objs); 
+            }
+        })(reader);
+
+        reader.readAsText(file);
+
+	}
+	
+	onMount(() => {
+	
+	})
+	
+</script>
+
+<input type="file" id="input" on:change={(evt) => handleFiles(evt)} multiple>
